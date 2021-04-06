70627408a6d54caad498d2c9449695b5e8488e6a
uni2237 <a01022371341@gmail.com>
2021-04-07 00:51:30 +0900

    이코테-그리디-숫자 카드 게임

---
+++ 이코테 책/그리디/숫자카드게임.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open("Algorithm\input.txt","rt")
+input = sys.stdin.readline
+
+n,m=map(int,input().split())
+a=min(list(map(int,input().split())))
+for _ in range(n-1):
+    new=min(list(map(int,input().split())))
+    a = (a > new) and a or new  # 파이썬 삼항연산자 -> ? 대신 and, : 대신 or 사용
+    # a = max(a,new)  이렇게 해도 됨.
+print(a)
