f3df062def73f37f31a1685877f095380fbde0ba
uni2237 <a01022371341@gmail.com>
2021-04-07 00:58:00 +0900

    이코테-그리디-숫자카드게임

--- 이코테 책/그리디/숫자카드게임.py
+++ 이코테 책/그리디/숫자카드게임.py
@@ -7,5 +7,6 @@ a=min(list(map(int,input().split())))
 for _ in range(n-1):
     new=min(list(map(int,input().split())))
     a = (a > new) and a or new  # 파이썬 삼항연산자 -> ? 대신 and, : 대신 or 사용
+                                # 혹은 a = a if (a>new) else new
     # a = max(a,new)  이렇게 해도 됨.
 print(a)
