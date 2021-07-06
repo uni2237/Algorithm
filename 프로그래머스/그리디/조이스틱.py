945018bdf6776d4375bf7f02d8f274b4f5bdde03
Choyunhui <a01022371341@gmail.com>
2021-07-06 21:56:16 +0900

    프로그래머스-그리디-조이스틱

---
+++ 프로그래머스/그리디/조이스틱.py
@@ -0,0 +1,15 @@
+def solution(name):
+    answer = 0
+    min_move = len(name) - 1
+    next = 0
+    
+    for i, char in enumerate(name):
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+        
+        min_move = min(min_move, i + i + len(name) - next)
+    answer += min_move
+    return answer
