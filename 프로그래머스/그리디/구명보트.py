f53c76bd10efcfd67302e0ca1147d4e7bbcdd2ea
uni2237 <a01022371341@gmail.com>
2021-07-09 21:09:39 +0900

    프로그래머스-그리디-구명보트

--- 프로그래머스/그리디/구명보트.py
+++ 프로그래머스/그리디/구명보트.py
@@ -1,10 +1,6 @@
 def solution(people, limit):
     
-<<<<<<< HEAD
-    answer = 1
-=======
     answer = len(people)
->>>>>>> 33610f7d6e0dbc1db70a19a86a6e95852ed71e3d
     people.sort()
 
     i=0
@@ -13,25 +9,9 @@ def solution(people, limit):
     while i<j :
 
         if people[i]+people[j]<=limit:
-<<<<<<< HEAD
-            
-            i+=1
-            
-        else:
-            answer+=1
-            
-=======
             answer-=1
             i+=1
             
->>>>>>> 33610f7d6e0dbc1db70a19a86a6e95852ed71e3d
         j-=1
         
     return answer
-
-<<<<<<< HEAD
-print(solution([70, 50, 80, 50],100))
-=======
-
->>>>>>> 33610f7d6e0dbc1db70a19a86a6e95852ed71e3d
-
