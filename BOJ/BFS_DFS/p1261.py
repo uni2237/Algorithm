00853e64a6f755a5226a4ddd32e4833cfdcb4fc6
uni2237 <a01022371341@gmail.com>
2021-07-31 00:19:19 +0900

    백준 1261번 알고스팟-bfs 수정

--- BOJ/BFS_DFS/p1261.py
+++ BOJ/BFS_DFS/p1261.py
@@ -1,28 +1,33 @@
 import sys
 sys.stdin=open("Algorithm/input.txt","rt")
-input = sys.stdin.readline
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+array=[[int(i) for i in input().rstrip()] for _ in range(n)]
+
+visited=[[-1]*m for _ in range(n)]
+print(array)
+print(visited)
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
 from collections import deque
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-m,n = map(int, input().split())
-arr = [ list(map(int, input().rstrip())) for _ in range(n)]
-visited = [[-1] * m for _ in range(n)]  
-
-queue = deque()
+queue=deque()
 queue.append((0,0))
-visited[0][0] = 0
+visited[0][0]=0
+
 while queue:
-    x,y = queue.popleft()
+    x,y=queue.popleft()
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if visited[nx][ny] == -1:
-                if not arr[nx][ny] :
-                    visited[nx][ny] = visited[x][y]
-                    queue.appendleft((nx, ny))
-                else:
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.append((nx, ny))
+        nx,ny=x+dx[i],y+dy[i]
+        if 0<=nx<n and 0<=ny<m:
+            if visited[nx][ny]==-1: # 아직 방문 x
+                if array[nx][ny]==1: # 벽일 때
+                    visited[nx][ny]=visited[x][y]+1
+                    queue.append((nx,ny)) # 큐 뒤에 추가
+                else: # 빈방일때
+                    visited[nx][ny]=visited[x][y]
+                    queue.appendleft((nx,ny)) # 큐 앞에 추가
 print(visited[n-1][m-1])
+
