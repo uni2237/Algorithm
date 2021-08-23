becbdf3ff9ed1faeed6f6c0f5f0540168ac9ade9
uni2237 <yunhui.cho98@gmail.com>
2021-08-23 22:02:07 +0900

    이코테-그리디-숫자 카드 게임_ 수정

--- 이코테 책/0_그리디/숫자카드게임.py
+++ 이코테 책/0_그리디/숫자카드게임.py
@@ -1,12 +1,19 @@
 import sys
-sys.stdin = open("Algorithm\input.txt","rt")
+sys.stdin = open("input.txt","rt")
 input = sys.stdin.readline
 
 n,m=map(int,input().split())
-a=min(list(map(int,input().split())))
-for _ in range(n-1):
-    new=min(list(map(int,input().split())))
-    a = (a > new) and a or new  # 파이썬 삼항연산자 -> ? 대신 and, : 대신 or 사용
-                                # 혹은 a = a if (a>new) else new
-    # a = max(a,new)  이렇게 해도 됨.
-print(a)
+
+# 버전 1 : 각 행의 min값을 모두 저장해둔다음 한번에 max값 구하기
+data=[]
+for i in range(n):
+    data.append(min(list(map(int,input().split()))))
+print(max(data))
+
+
+# 버전 2 : 매 행 마다 직전 행의 최솟값과 비교하기
+result=0
+for i in range(n):
+    target=min(list(map(int,input().split())))
+    result=max(target,result)
+print(result)
