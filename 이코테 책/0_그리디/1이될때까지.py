ba16968e7a088675e27c7f054f89c5a40e31492d
uni2237 <yunhui.cho98@gmail.com>
2021-08-23 22:39:30 +0900

    이코테-그리디-1이 될때까지 _수정

--- 이코테 책/0_그리디/1이될때까지.py
+++ 이코테 책/0_그리디/1이될때까지.py
@@ -1,13 +1,23 @@
 import sys
-sys.stdin = open("Algorithm\input.txt","rt")
+sys.stdin = open("input.txt","rt")
 input = sys.stdin.readline
 n,k=map(int,input().split())
 
-count=1
+count=0
 
-while k**count <=n:
-    count+=1
+while True: # k로 최대한 많이 나눠야함!
+    tmp=(n//k)*k 
+    count+=n-tmp # k배수로 만들고 남은만큼 다 +1씩,
+    n=tmp # n은 k배수로 갱신
+
+    if n<k: # n이 k보다 작아져 더 나눌수 없음
+        break
     
-print(n-(k**(count-1))+(count-1))
+    n=n//k # k배수가 된 n을 k로 나눈 몫으로 갱신
+    count+=1 # k로 나눠 줬으니까 +1 해줌
 
-# 나누기 부터 계속 하는게 그리디 ! 나는 제곱으로 쉽게 구했음.
+count+=n-1 # k로 못 나누고 남은 만큼 +1씩 해줌.
+print(count)
+
+
+
