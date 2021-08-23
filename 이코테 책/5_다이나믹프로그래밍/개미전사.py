d3343a1170e06c4b6e9c80699924460376d2f494
uni2237 <yunhui.cho98@gmail.com>
2021-08-23 21:26:25 +0900

    이코테-dp-개미전사

---
+++ 이코테 책/5_다이나믹프로그래밍/개미전사.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin=open("input.txt","rt")
+input=sys.stdin.readline
+
+n=int(input())
+arr=list(map(int,input().split()))
+
+d=[0]*100
+
+d[0]=arr[0]
+d[1]=max(arr[0],arr[1])
+
+for i in range(2,n):
+    d[i]=max(d[i-1],d[i-2]+arr[i])
+
+print(d[n-1])
