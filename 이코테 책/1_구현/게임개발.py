eb708e1f475a4265ef1c75fd0f40d82db8f85084
uni2237 <yunhui.cho98@gmail.com>
2021-08-24 16:36:10 +0900

    이코테-구현-게임개발_수정

--- 이코테 책/1_구현/게임개발.py
+++ 이코테 책/1_구현/게임개발.py
@@ -1,74 +1,54 @@
 import sys
-sys.stdin = open("Algorithm\input.txt","rt")
+from typing import MappingView
+sys.stdin = open("input.txt","rt")
 input = sys.stdin.readline
 
-
-#북동남서
-dx=[-1,0,+1,0]
-dy=[0,+1,0,-1]
-
 n,m=map(int,input().split())
-d=[[0]*m for _ in range(n)]
+x,y,d=map(int,input().split())
 
+map=list(list(map(int,input().split())) for _ in range(n))
+visit=[[0]*m for _ in range(n)]
 
-x,y,direction=map(int,input().split())
-d[x][y]=1
+# 북서남동
+dx=[-1,0,1,0]
+dy=[0,-1,0,1]
 
-arr=[]
-for i in range(n):
-    arr.append(list(map(int,input().split())))
-
-def turn_left():
-    # 북(0)->서(3)
-    # 동(1)->북(0)
-    # 남(2)->동(1)
-    # 서(3)->남(2)  
-    global direction  # 함수 밖에서 global로 전역변수 선언해도, 함수 안에서 global 명시 안하면 지역변수로 처리됨 !!!!!!  함수안에서 global 명시 해주자!
-    direction= (direction+3)%4
-    
-
+rotate_count=0
+visit[x][y]=1
 count=1
-no_turn=0
 
 while True:
-    turn_left() #왼쪽 회전
-    nx=x+dx[direction]
-    ny=y+dy[direction]
-
-    # 아직 방문 x 
-    if d[nx][ny]==0 and arr[nx][ny]==0:
-        d[nx][ny]=1
-        count+=1
-        x=nx
-        y=ny
-        no_turn=0
-        continue
-
-    # 방문했거나 바다
-    else:
-        no_turn+=1
     
-    # 4번 다 갈곳 x
-    if no_turn==4:
-        nx=x-dx[direction]
-        ny=y-dy[direction]
+    d = d-1 if (d!=0) else 3
+    nx,ny=x+dx[d],y+dy[d]
+    
+    # 회전한 이후, 정면에 안 가본 칸 있을 때
+    if map[nx][ny]==0 and visit[nx][ny]==0 :
+        visit[nx][ny]=1
+        rotate_count=0
+        count+=1
+        x,y=nx,ny
+        continue
+    else: # 회전한 이후, 가보지않은 칸이 없거나 바다인경우
+        rotate_count+=1
 
-        # 뒤가 육지, 갈수있음
-        if arr[nx][ny]==0:
-            x=nx
-            y=ny
-        # 뒤가 바다라 못감
+    # 네 방향 모두 못감
+    if rotate_count==4 :
+        
+        # 뒤로 이동 시 
+        behind=(d+2)%4
+        nx,ny=x+dx[behind],y+dy[behind]
+        # behind 안쓰고 바로 nx,ny= x-dx[d], y-dy[d] 이렇게 해도 됨.
+        
+        # 뒤로 갈수있음
+        if map[nx][ny]==0:
+            x,y=nx,ny # 이미 방문한곳이므로 x,y만 변경해주고, visitd와 count는 변경 팔요 x 
+        # 뒤가 바다라 못감  
         else:
             break
-        
-        no_turn = 0 # 다시 0으로 초기화
+        rotate_count=0
+            
+    
+    
 
 print(count)
-
-#입력
-#4 4 
-#1 1 0 
-#1 1 1 1 
-#1 0 0 1 
-#1 1 0 1 
-#1 1 1 1
