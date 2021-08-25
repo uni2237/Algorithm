28040ec5002b2e52046fd69b4abe4f93c3f8e7f9
uni2237 <yunhui.cho98@gmail.com>
2021-08-26 00:39:49 +0900

    이코테-정렬-두 배열의 원소 교체_수정

--- 이코테 책/3_정렬/두 배열의 원소 교체.py
+++ 이코테 책/3_정렬/두 배열의 원소 교체.py
@@ -1,19 +1,12 @@
 import sys
-sys.stdin = open("Algorithm\input.txt","rt")
+sys.stdin = open("input.txt","rt")
 input=sys.stdin.readline
 
-n,k = map(int,input().split())
-a= list(map(int,input().split()))
-b= list(map(int,input().split()))
-
-a.sort()
-b.sort(reverse=True)
-
+n,k=map(int,input().split())
+a=sorted(list(map(int,input().split())))
+b=sorted(list(map(int,input().split())),reverse=True)
 
 for i in range(k):
-    if b[i]>a[i] :
-        a[i],b[i]=b[i],a[i] # swap
-    else:
-        break
-
+    if b[i]>a[i]:
+        a[i],b[i]=b[i],a[i]
 print(sum(a))
