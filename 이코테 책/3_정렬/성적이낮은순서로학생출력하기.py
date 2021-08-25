a9df882c7723b529c61af721b11cf1080d6fd4a4
uni2237 <yunhui.cho98@gmail.com>
2021-08-26 00:34:39 +0900

    이코테-정렬-성적이 낮은 순서로 학생 출력하기 _ 수정

--- 이코테 책/3_정렬/성적이낮은순서로학생출력하기.py
+++ 이코테 책/3_정렬/성적이낮은순서로학생출력하기.py
@@ -1,24 +1,19 @@
 import sys
-sys.stdin = open("Algorithm\input.txt","rt",encoding="UTF8")
+sys.stdin = open("input.txt","rt",encoding="UTF8")
 input=sys.stdin.readline
 
 n=int(input())
-
-array=[]
-for i in range(n):
-    arr=input().split()
-    array.append((arr[0],int(arr[1])))
-
+student=[]
+for _ in range(n):
+    key,value=input().split()
+    student.append([key,int(value)])
+'''
 def setting(data):
     return data[1]
 
-array = sorted(array, key=setting)
+student = sorted(student, key=setting)
 '''
-lambda 사용버전
-array=sorted(array,key=lambda data:data[1])
-'''
-for a in array:
-    print(a[0],end=" ")
 
-# lambda 사용버전
-array=sorted(array,key=lambda data:data[1])
+for s in sorted(student,key=lambda student:student[1]):
+    print(s[0],end=' ')
+
