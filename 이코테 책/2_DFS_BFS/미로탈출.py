06b1b34a27f64f6de0746bb026fdc7587dac3ccd
uni2237 <yunhui.cho98@gmail.com>
2021-08-25 18:34:38 +0900

    이코테-bfs-미로탈출_수정

--- 이코테 책/2_DFS_BFS/미로탈출.py
+++ 이코테 책/2_DFS_BFS/미로탈출.py
@@ -1,36 +1,33 @@
 import sys
-sys.stdin = open("Algorithm\input.txt","rt")
+sys.stdin = open("input.txt","rt")
 input=sys.stdin.readline
 
 n,m=map(int,input().split())
 
-graph=[ [int(x) for x in input().rstrip()] for _ in range(n) ]
+maze=[ [int(x) for x in input().rstrip()] for _ in range(n) ]
 
 from collections import deque
-queue=deque()
+que=deque()
 
-#상,하,좌,우
+
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
 def bfs(x,y):
-    queue.append((x,y))
+    que.append((x,y))
 
-    while queue:
-        x,y=queue.popleft()
-
+    while que:
+        x,y=que.popleft()
+        
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            
+            nx,ny=x+dx[i],y+dy[i]
+
             if nx<0 or nx>=n or ny<0 or ny>=m:
                 continue
-            if graph[nx][ny]==0:
+            elif maze[nx][ny]==0:
                 continue
-            
-            if graph[nx][ny]==1:
-                graph[nx][ny]=graph[x][y]+1
-                queue.append((nx,ny))
-    return graph[n-1][m-1]
-
+            elif maze[nx][ny]==1:
+                maze[nx][ny]=maze[x][y]+1
+                que.append((nx,ny))
+    return maze[n-1][m-1]
 print(bfs(0,0))
