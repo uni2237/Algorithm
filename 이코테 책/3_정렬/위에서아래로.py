8cd990108b0fb12deb82417c67306a6bdc150bf2
uni2237 <yunhui.cho98@gmail.com>
2021-08-26 00:24:41 +0900

    이코테-정렬-위에서 아래로_수정

--- 이코테 책/3_정렬/위에서아래로.py
+++ 이코테 책/3_정렬/위에서아래로.py
@@ -1,12 +1,10 @@
 import sys
-sys.stdin = open("Algorithm/input.txt","rt")
+sys.stdin = open("input.txt","rt")
 input=sys.stdin.readline
 
-n  = int(input())
-
+n=int(input())
 
 a=[]
-
 for _ in range(n):
     a.append(int(input()))
 
