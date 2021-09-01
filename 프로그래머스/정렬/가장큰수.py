5d008feeb6753270fae78dda00367a6d61ea9e67
uni2237 <yunhui.cho98@gmail.com>
2021-09-01 13:03:36 +0900

    프로그래머스-가장큰수-정렬

---
+++ 프로그래머스/정렬/가장큰수.py
@@ -0,0 +1,28 @@
+# version 1 - cmp_to_key 사용 / 두 수를 조합해서 큰 수가 나오는 순서대로 정렬
+# a , b 일 때 문자열 조합 시  a+b > b+a 이면 a , b 순으로 정렬 
+# ex) 9 , 90 -> a+b : 990 , b+a : 909 => 990이 909보다 큼(사전 순) -> 9가 우선순위
+from functools import cmp_to_key
+def comparator(a,b):
+    t1,t2 = a+b, b+a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2))  # t1이 크다면 1, t2가 크다면 -1,  같으면 0
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=cmp_to_key(comparator), reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
+
+# version 2 - 숫자들을 문자열로 바꾼것을 *3해서 (1000이하니까) 비교 
+#  ex) 9 , 90  -> 999, 909090 이므로 999가 더 큼(사전 순) -> 9가 우선순위
+def solution(numbers): 
+    numbers = list(map(str, numbers)) 
+    numbers.sort(key=lambda x: x*3, reverse=True) 
+    return str(int(''.join(numbers)))
+
+# 내 풀이 - permutations 사용  / 다 시간초과.. ㅋ
+import itertools
+def solution(numbers): 
+    numbers=list(map(str,numbers))
+    a=list(map(''.join, itertools.permutations(numbers)))
+    print(sorted(a)[-1])
+solution([6,10,2])
