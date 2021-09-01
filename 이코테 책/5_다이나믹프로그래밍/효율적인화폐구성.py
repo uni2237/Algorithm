073978137d4265b476bf5f1dac4ef7ea60408e71
uni2237 <yunhui.cho98@gmail.com>
2021-09-02 01:31:34 +0900

    이코테-다이나믹프로그래밍-효율적인 화폐구성_수정

--- 이코테 책/5_다이나믹프로그래밍/효율적인화폐구성.py
+++ 이코테 책/5_다이나믹프로그래밍/효율적인화폐구성.py
@@ -1,18 +1,19 @@
-# 정수 N, M을 입력받기
-n, m = map(int, input().split())
-# N개의 화폐 단위 정보를 입력받기
-array = []
-for i in range(n):
-array.append(int(input()))
-# 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
-d = [10001] * (m + 1)
-# 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
-d[0] = 0
-for i in range(n):
-for j in range(array[i], m + 1):
-if d[j - array[i]] != 10001: # (i - k)원을 만드는 방법이 존재하는 경우
-d[j] = min(d[j], d[j - array[i]] + 1)
-# 계산된 결과 출력
-if d[m] == 10001: # 최종적으로 M원을 만드는 방법이 없는 경우
-print(-1) else:
-print(d[m])
+import sys
+sys.stdin=open("input.txt","rt")
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+money=[]
+for _ in range(n):
+    money.append(int(input()))
+
+# 화폐 단위들이 큰단위가 작은 단위의 배수관계가 아니므로, 그리디처럼 큰 화폐단위부터 처리하는 방법 안 통함!!!!
+d=[10001]*(m+1)
+d[0]=0
+for coin in money:
+    for j in range(coin,m+1):
+        d[j]=min(d[j],d[j - coin]+1)
+if d[m]==10001:
+    print(-1)
+else:
+    print(d[m])
