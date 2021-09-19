28408cfc9b8f8dd86bd437d30bb2a4f9c6d408f2
uni2237 <yunhui.cho98@gmail.com>
2021-09-20 00:30:06 +0900

    프로그래머스-정렬-h index

---
+++ 프로그래머스/정렬/h-인덱스.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    citations.sort()
+    n = len(citations)
+    for i in range(n):
+        if citations[i] >= n-i:
+            return n-i
+    return 0
+print(solution([5,5]))
